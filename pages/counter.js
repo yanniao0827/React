939cc6a5b86d10e16a96f63385bfc29cde164c9c
yanniao0827 <yanning0113niao@gmail.com>
2024-06-03 15:26:54 +0800

    react的計數器

--- pages/counter.js
+++ pages/counter.js
@@ -1,10 +1,20 @@
+// useState是一種鉤子hooks，是設計來讓韓式行元件可以使用狀態的，如useState、useEffect
 import { useState } from 'react'
 
 export default function Counter() {
+  // useState(0)讓計數器從0開始
+  const [total, setTotal] = useState(0)
   return (
     <>
-      <h1>0</h1>
-      <button>+1</button>
+      <h1>{total}</h1>
+      <button
+        //   在一班的JS中，設計功能是用addEventListener，但是在react中會使用許多人造事件屬性，如這裡的onClick
+        onClick={() => {
+          setTotal(total + 1)
+        }}
+      >
+        +1
+      </button>
     </>
   )
 }
