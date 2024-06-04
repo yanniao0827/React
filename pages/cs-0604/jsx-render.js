b7e3638617e4d5b3641d7bdae2293bcb35f63574
yanniao0827 <yanning0113niao@gmail.com>
2024-06-04 09:56:43 +0800

    react中的陣列、物件、函式渲染

--- pages/cs-0604/jsx-render.js
+++ pages/cs-0604/jsx-render.js
@@ -16,12 +16,31 @@ export default function JsxRender() {
       {`price=${100 - 1}`}
       <hr />
       <h2>Boolean data</h2>
+      {/* 會無法渲染顯示不出來，但不會跳錯誤訊息 */}
       {true}
       {false}
       <hr />
       <h2>Null/Umdefine data</h2>
+      {/* 會無法渲染顯示不出來，但不會跳錯誤訊息 */}
       {null}
       {undefined}
+      <hr />
+      <h2>Array data</h2>
+      {/* 陣列內容會被串接成字串 */}
+      {[1, 2, 3]}
+      {['hello', 'Lea']}
+      {/* key就跟html的id一樣 */}
+      {[<li key="1">abc</li>, <li key="2">def</li>]}
+      <hr />
+      <h2>object(物件)</h2>
+      {/* 不能直接渲染，會造成中斷錯誤，不是React Child */}
+      {/* 參考: https://github.com/orgs/mfee-react/discussions/91 */}
+      {/* {{ a: 1, b: 2 }} */}
+      <hr />
+      <h2>function(函式)</h2>
+      {/* 不會渲染，會有主控台警告，不是React Child */}
+      {/* 參考: https://github.com/orgs/mfee-react/discussions/91 */}
+      {/* {() => {}} */}
       <hr />
     </>
   );
