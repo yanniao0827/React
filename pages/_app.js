50d8d1b7a7d220d9744ffb05b1470bf07c05369c
yanniao0827 <yanning0113niao@gmail.com>
2024-06-05 09:36:17 +0800

    style jsx

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 // import '@/styles/globals.css'
-import '@/styles/product-table.css';
+// import '@/styles/product-table.css';
 
 export default function MyApp({ Component, pageProps }) {
   // 使用自訂在頁面層級的版面(layout)
