a70801d0778f923e62ff96130d011009cc35602c
yanniao0827 <yanning0113niao@gmail.com>
2024-06-07 14:38:30 +0800

    屬性預設值

--- components/props_a/child.js
+++ components/props_a/child.js
@@ -1,16 +1,24 @@
 import React from 'react';
 
-export default function Child(props) {
+export default function Child({
+  // 在這裡可以設定預設值，如果父母元件沒有給值的話就會套用預設值，可以見那邊查看資料類型是什麼
+  title = '沒設定',
+  price = 0,
+  isConnected = false,
+  aa = [],
+  oa = {},
+  sum = () => {},
+}) {
   return (
     <>
       <h3>Child</h3>
       {/* 這裡是將父母元件給的屬性顯示出來 */}
-      <p>title={props.title}</p>
-      <p>price = {props.price}</p>
-      <p>isConnected = {props.isConnected ? 'true' : 'false'}</p>
-      <p>aa = {JSON.stringify(props.aa)}</p>
-      <p>oa = {JSON.stringify(props.oa)}</p>
-      <p>sum(1,2) = {props.sum(1, 2)}</p>
+      <p>title={title}</p>
+      <p>price = {price}</p>
+      <p>isConnected = {isConnected ? 'true' : 'false'}</p>
+      <p>aa = {JSON.stringify(aa)}</p>
+      <p>oa = {JSON.stringify(oa)}</p>
+      <p>sum() = {sum(1, 2)}</p>
     </>
   );
 }
--- components/props_a/parent.js
+++ components/props_a/parent.js
@@ -5,14 +5,14 @@ export default function Parent() {
   return (
     <>
       <h2>Parent</h2>
-      {/* 父母元件設定屬性給子女元件 */}
+      {/*  */}
       <Child
-        title="0607 props"
-        price={555}
-        isConnected={true}
-        aa={[1, 2, 3]}
-        oa={{ a: 1, b: 2 }}
-        sum={(x, y) => x + y}
+      // title="0607 props"
+      // price={555}
+      // isConnected={true}
+      // aa={[1, 2, 3]}
+      // oa={{ a: 1, b: 2 }}
+      // sum={(x, y) => x + y}
       />
     </>
   );
