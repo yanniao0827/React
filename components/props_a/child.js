dff0cba49197b9204a50ecdfbf7b978213772b0b
yanniao0827 <yanning0113niao@gmail.com>
2024-06-07 14:09:45 +0800

    父母設定屬性給子女

--- components/props_a/child.js
+++ components/props_a/child.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
-export default function Child() {
+export default function Child(props) {
   return (
     <>
       <h3>Child</h3>
+      {/* 這裡是將父母元件給的屬性顯示出來 */}
+      <p>title={props.title}</p>
+      <p>price = {props.price}</p>
+      <p>isConnected = {props.isConnected ? 'true' : 'false'}</p>
+      <p>aa = {JSON.stringify(props.aa)}</p>
+      <p>oa = {JSON.stringify(props.oa)}</p>
+      <p>sum(1,2) = {props.sum(1, 2)}</p>
     </>
   );
 }
