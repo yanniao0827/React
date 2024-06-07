602c51806ca895027d626e5bbaa1b5d08c33166e
yanniao0827 <yanning0113niao@gmail.com>
2024-06-07 11:13:04 +0800

    星星套用按鈕方式

--- pages/cs-0607/star.js
+++ pages/cs-0607/star.js
@@ -1,18 +1,23 @@
-import React from 'react'
+import React from 'react';
 // 導入.module.css檔案
-import styles from '@/styles/star.module.css'
+import styles from '@/styles/star.module.css';
 
 export default function Star() {
   return (
     <>
       <h1>星星評分範例</h1>
       <div>
-        <span className={styles['good-style']}>&#9733;</span>
-        <span>&#9733;</span>
-        <span>&#9733;</span>
-        <span>&#9733;</span>
-        <span>&#9733;</span>
+        {Array(5)
+          .fill(1)
+          .map((v, i) => {
+            return (
+              <button key={i} className={styles['star-btn']}>
+                {' '}
+                <span>&#9733;</span>
+              </button>
+            );
+          })}
       </div>
     </>
-  )
+  );
 }
