11164401b0d3f02cb08f773cae577a89e0267928
yanniao0827 <yanning0113niao@gmail.com>
2024-06-07 11:34:39 +0800

    完成星星評分

--- pages/cs-0607/star.js
+++ pages/cs-0607/star.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import { useState } from 'react';
 // 導入.module.css檔案
 import styles from '@/styles/star.module.css';
 
 export default function Star() {
+  const [rating, setRating] = useState(0);
   return (
     <>
       <h1>星星評分範例</h1>
@@ -10,13 +11,25 @@ export default function Star() {
         {Array(5)
           .fill(1)
           .map((v, i) => {
+            const score = i + 1;
             return (
-              <button key={i} className={styles['star-btn']}>
+              <button
+                key={i}
+                className={styles['star-btn']}
+                onClick={() => {
+                  setRating(score);
+                }}
+              >
                 {' '}
-                <span>&#9733;</span>
+                <span
+                  className={score <= rating ? styles['on'] : styles['off']}
+                >
+                  &#9733;
+                </span>
               </button>
             );
           })}
+        <p>你給了{rating}顆星</p>
       </div>
     </>
   );
