e8bea97cf29c9fe987428868d03e86bf1a101589
yanniao0827 <yanning0113niao@gmail.com>
2024-06-07 10:09:58 +0800

    設定表格初始狀態

--- pages/cs-0607/book-list.js
+++ pages/cs-0607/book-list.js
@@ -1,15 +1,21 @@
-import { useState } from 'react'
-import Image from 'next/image'
+import { useState } from 'react';
+import Image from 'next/image';
 
 // 範例資料
-import data from '@/data/books.json'
+import data from '@/data/books.json';
 
 // 實心圖
-import bookmarkIconFill from '@/assets/bookmark-fill.svg'
+import bookmarkIconFill from '@/assets/bookmark-fill.svg';
 // 空心圖
-import bookmarkIcon from '@/assets/bookmark.svg'
+import bookmarkIcon from '@/assets/bookmark.svg';
 
 export default function BookList() {
+  // 設定表格的初始狀態，原本的json資料裡面沒有收藏欄，所以用map的方法先抓books資料庫面的資料並且加上fav欄位，並將該欄位的初始狀態設定成false
+  const initState = data.map((v, i) => {
+    return { ...v, fav: false };
+  });
+
+  const [books, setBooks] = useState(initState);
   return (
     <>
       <h1>書籍清單</h1>
@@ -23,16 +29,20 @@ export default function BookList() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>XXX</td>
-            <td>XXX</td>
-            <td>XXX</td>
-            <td>
-              <Image src={bookmarkIcon} alt="" />
-            </td>
-          </tr>
+          {books.map((v, i) => {
+            return (
+              <tr key={v.isbn}>
+                <td>{v.isbn}</td>
+                <td>{v.title}</td>
+                <td>{v.author}</td>
+                <td>
+                  <Image src={bookmarkIcon} alt="" />
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </>
-  )
+  );
 }
