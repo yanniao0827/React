63cd6a4b3465eb186cf9085ed9b9536579986845
yanniao0827 <yanning0113niao@gmail.com>
2024-06-11 12:03:34 +0800

    預設星值改寫

--- pages/cs-0611/star-page.js
+++ pages/cs-0611/star-page.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import { useState } from 'react';
 
 import Star from '@/components/stars';
 
 export default function StarPage() {
+  const [productRating1, setProductRating1] = useState(2);
+  const [productRating2, setProductRating2] = useState(1);
   return (
     <>
       <h1>星星評分元件測試頁</h1>
@@ -11,8 +13,16 @@ export default function StarPage() {
       <Star />
       <hr />
       {/* 測試組，自行設定最高跟最一開始的星數，也就是parent設定的值 */}
-      <Star maxCount={3} initRating={1} />
-      <Star maxCount={6} initRating={5} />
+      <Star
+        maxCount={3}
+        initRating={productRating1}
+        onRatingChange={setProductRating1}
+      />
+      <Star
+        maxCount={4}
+        initRating={productRating2}
+        onRatingChange={setProductRating2}
+      />
     </>
   );
 }
