3b4e231e9b0dd2f3d843ec3f2376a6230fd4db11
yanniao0827 <yanning0113niao@gmail.com>
2024-06-12 11:13:14 +0800

    導入product資料

--- components/checkout/product-list.js
+++ components/checkout/product-list.js
@@ -1,26 +1,23 @@
-import React from 'react'
-import data from '@/data/Product.json'
-import styles from './cart.module.css'
+import React from 'react';
+import products from '@/data/Product.json';
+import styles from './cart.module.css';
 
 export default function ProductList() {
   return (
     <>
       <ul className={styles['list']}>
-        <li className={styles['item']}>
-          <div className={styles['w-400']}>XXXXXX</div>
-          <div>10000</div>
-          <div>
-            <button onClick={() => {}}>加入購物車</button>
-          </div>
-        </li>
-        <li className={styles['item']}>
-          <div className={styles['w-400']}>XXXXXX</div>
-          <div>10000</div>
-          <div>
-            <button onClick={() => {}}>加入購物車</button>
-          </div>
-        </li>
+        {products.map((v, i) => {
+          return (
+            <li key={v.id} className={styles['item']}>
+              <div className={styles['w-400']}>{v.name}</div>
+              <div>{v.price}</div>
+              <div>
+                <button onClick={() => {}}>加入購物車</button>
+              </div>
+            </li>
+          );
+        })}
       </ul>
     </>
-  )
+  );
 }
