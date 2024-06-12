59d18048e3d8a5fef60e8a514f499d7de2048c71
yanniao0827 <yanning0113niao@gmail.com>
2024-06-12 15:21:09 +0800

    好看的module

--- components/checkout/cart-list.js
+++ components/checkout/cart-list.js
@@ -1,10 +1,36 @@
 import React from 'react';
 import styles from './cart.module.css';
 import { useCart } from '@/hooks/use-cart';
+// import Swal from 'sweetalert2';
+// import withReactContent from 'sweetalert2-react-content';
 
 export default function CartList() {
   const { items, increaseItem, decreaseItem, removeItem } = useCart();
 
+  // const MySwal = withReactContent(Swal);
+  // const notifyAndRemove = (productName, productId) => {
+  //   MySwal.fire({
+  //     title: '你確定嗎?',
+  //     text: '你將無法回復這個操作',
+  //     icon: 'warning',
+  //     showCancelButton: true,
+  //     confirmButtonColor: '#3085d6',
+  //     cancelButtonColor: '#d33',
+  //     cancelButtonText: '取消',
+  //     confirmButtonText: '確定刪除!',
+  //   }).then((result) => {
+  //     if (result.isConfirmed) {
+  //       MySwal.fire({
+  //         title: '已刪除!',
+  //         text: productName + '已經被刪除',
+  //         icon: 'success',
+  //       });
+
+  //       // 作刪除的動作
+  //       removeItem(productId);
+  //     }
+  //   });
+  // };
   return (
     <>
       <div className={styles['cart']}>
