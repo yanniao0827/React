b431bac61b6c07df7573d8c0e644b703bdf79633
yanniao0827 <yanning0113niao@gmail.com>
2024-06-12 10:17:26 +0800

    useRouter(用alert切換頁面)

--- pages/cs-0612/context-a/page2.js
+++ pages/cs-0612/context-a/page2.js
@@ -1,8 +1,10 @@
 import List from '@/components/context-a/list';
 // 用於取代a元件的特別元件，可以在不同頁面保持住狀態
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 export default function Page2() {
+  const router = useRouter();
   return (
     <>
       <h1>Page2(Context範例-a)</h1>
@@ -12,6 +14,18 @@ export default function Page2() {
       <br />
       {/*  用於取代a元件的特別元件，可以在不同頁面保持住狀態 */}
       <Link href="/cs-0612/context-a/page1">Page1(Link元件)</Link>
+      <br />
+      <button
+        onClick={() => {
+          alert('welcome');
+          if (confirm('sure?')) {
+            // 連至page1
+            router.push('/cs-0612/context-a/page1');
+          }
+        }}
+      >
+        Page1
+      </button>
     </>
   );
 }
