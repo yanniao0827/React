d84b0ba4c152e24f7ad0c8aa0ac158366e610115
yanniao0827 <yanning0113niao@gmail.com>
2024-07-01 09:59:24 +0800

    可控表單元件範例 解決組字過程

--- pages/cs-0701/controlled-from.js
+++ pages/cs-0701/controlled-from.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
+import InputIME from '@/components/controlled-form/input-ime';
 
 export default function ControlledForm() {
   const [inputText, setInputText] = useState('');
@@ -8,7 +9,16 @@ export default function ControlledForm() {
       <h1>可控表單元件範例</h1>
       <hr />
       <div title="input-text">
+        <h2>文字輸入框(input-text)</h2>
         <input
+          type="text"
+          value={inputText}
+          onChange={(e) => {
+            setInputText(e.target.value);
+          }}
+        />
+        <h2>文字輸入框(input-text)-修正輸入法組字</h2>
+        <InputIME
           type="text"
           value={inputText}
           onChange={(e) => {
