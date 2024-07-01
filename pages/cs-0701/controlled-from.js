0a6358cf3130a8dafe576b310ab2c31b2663ebcc
yanniao0827 <yanning0113niao@gmail.com>
2024-07-01 13:52:11 +0800

    可控表單 下拉選單

--- pages/cs-0701/controlled-from.js
+++ pages/cs-0701/controlled-from.js
@@ -5,14 +5,34 @@ import MyRadio from '@/components/controlled-form/my-radio';
 import MyCheckbox from '@/components/controlled-form/my-checkbox';
 
 export default function ControlledForm() {
+  const drinkOptions = ['高山金萱', '熟成紅茶', '胭脂多多', '蘋果紅宣'];
+  const [drink, setDrink] = useState('');
   return (
     <>
       <h1>可控表單元件範例</h1>
       <hr />
       {/* <MyInput />
       <MyTextarea /> */}
-      <MyRadio />
-      <MyCheckbox />
+      {/* <MyRadio />
+      <MyCheckbox /> */}
+      <div title="select">
+        <h2>下拉選單</h2>
+        <select
+          value={drink}
+          onChange={(e) => {
+            setDrink(e.target.value);
+          }}
+        >
+          <option value="">請選擇品項</option>
+          {drinkOptions.map((v, i) => {
+            return (
+              <option key={i} value={v}>
+                {v}
+              </option>
+            );
+          })}
+        </select>
+      </div>
     </>
   );
 }
