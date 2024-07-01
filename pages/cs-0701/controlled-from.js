ed21e1c73efc2252dbf86713653c85452366ea36
yanniao0827 <yanning0113niao@gmail.com>
2024-07-01 10:08:44 +0800

    可控表單 數字輸入欄以及日期(字串/物件)

--- pages/cs-0701/controlled-from.js
+++ pages/cs-0701/controlled-from.js
@@ -3,6 +3,16 @@ import InputIME from '@/components/controlled-form/input-ime';
 
 export default function ControlledForm() {
   const [inputText, setInputText] = useState('');
+  const [inputNumber, setInputNumber] = useState(0);
+
+  const [inputDateText, setInputDateText] = useState('');
+
+  const dateToString = (date = null) =>
+    date instanceof Date ? date.toISOString().split('T')[0] : '';
+  const stringToDate = (str = '') => new Date(str);
+  const [inputDateObject, setInputDateObject] = useState(
+    stringToDate('2024-07-01')
+  );
 
   return (
     <>
@@ -23,6 +33,33 @@ export default function ControlledForm() {
           value={inputText}
           onChange={(e) => {
             setInputText(e.target.value);
+          }}
+        />
+        <h2>數字輸入框(input-number)</h2>
+        <input
+          type="number"
+          max={10}
+          min={0}
+          step={1}
+          value={inputNumber}
+          onChange={(e) => {
+            setInputNumber(Number(e.target.value));
+          }}
+        />
+        <h2>日期輸入(input-date)字串</h2>
+        <input
+          type="date"
+          value={inputDateText}
+          onChange={(e) => {
+            setInputDateText(e.target.value);
+          }}
+        />
+        <h2>日期輸入(input-date)物件</h2>
+        <input
+          type="date"
+          value={dateToString(inputDateObject)}
+          onChange={(e) => {
+            setInputDateObject(stringToDate(e.target.value));
           }}
         />
       </div>
